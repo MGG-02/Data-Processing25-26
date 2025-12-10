dd8d1645620df037236c306c96e951f1787d3bca
ignahaguilar <ignahaguilar@gmail.com>
2025-12-10 13:47:33 +0100

    w2v - scikit (SVM, RF)

--- word2vec.py
+++ word2vec.py
@@ -32,17 +32,33 @@ X_w2v = np.array([tweet_vector(tweet_tokens,model_w2v) for tweet_tokens in token
 #####################################
 
 from sklearn.model_selection import train_test_split 
+from sklearn.metrics import classification_report, accuracy_score, roc_auc_score, f1_score
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+
+######## Logistic Regression ########
+
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import classification_report, accuracy_score
 
 # Label array ('True', 'False', 'Unverified')
 Y = pheme_df['target'].values
 
+# Label Encoding ('True' -> 0, 'False' -> 1, 'Unverified' -> 2)
+label_encoder = LabelEncoder()
+Y_encoded = label_encoder.fit_transform(Y)
+
 # 60% -> Training dataset, 40% -> Test/validation datasets
-X_train,X_temp,y_train,y_temp = train_test_split(X_w2v,Y,test_size=0.4,random_state=42,stratify=Y) #type: ignore
+X_train,X_temp,y_train,y_temp = train_test_split(X_w2v,Y_encoded,
+      test_size=0.4,random_state=42,stratify=Y_encoded) #type: ignore
 
 # Split test/validation dataset 50/50 (20% -> Test dataset, 20% -> Validation dataset)
-X_val,X_test,y_val,y_test = train_test_split(X_temp,y_temp,test_size=0.5,random_state=42,stratify=y_temp)
+X_val,X_test,y_val,y_test = train_test_split(X_temp,y_temp,
+      test_size=0.5,random_state=42,stratify=y_temp)
+
+# Standarize datasets
+scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_val = scaler.transform(X_val)
+X_test = scaler.transform(X_test)
 
 print("Scikit - Datasets size")
 print("Train:", X_train.shape, "\nVal:", X_val.shape, "\nTest:", X_test.shape)
@@ -64,22 +80,68 @@ y_test_pred = log_reg.predict(X_test)
 print("Test report (LogReg on W2V embeddings):")
 print(classification_report(y_test, y_test_pred))
 print("Test Accuracy:", accuracy_score(y_test, y_test_pred))
+print("Test Roc Auc Score: ", roc_auc_score(y_test, log_reg.predict_proba(X_test), multi_class='ovr'))
+print("Test F1 Score: ", f1_score(y_test, y_test_pred, average='micro'))
 
+################ SVM ################
+
+from sklearn.svm import LinearSVC
+from sklearn.calibration import CalibratedClassifierCV
+
+# SVM model
+Lclas = LinearSVC()
+
+# Model training with training dataset (X_train,y_train)
+clf = CalibratedClassifierCV(Lclas).fit(X_train, y_train)
+
+# Validation report over validation dataset (X_val,y_val)
+y_val_pred_svm = clf.predict(X_val)
+print("Validation report (SVM on W2V embeddings):")
+print(classification_report(y_val, y_val_pred_svm))
+print("Validation Accuracy:", accuracy_score(y_val, y_val_pred_svm))
+
+# Final evaluation over test dataset (X_test,y_test)
+y_test_pred_svm = clf.predict(X_test)
+print("Test report (SVM on W2V embeddings):")
+print(classification_report(y_test, y_test_pred_svm))
+print("Test Accuracy:", accuracy_score(y_test, y_test_pred_svm))
+print("Test Roc Auc Score: ", roc_auc_score(y_test, clf.predict_proba(X_test), multi_class='ovr'))
+print("Test F1 Score: ", f1_score(y_test, y_test_pred_svm, average='micro'))
+
+########### Random Forest ###########
+
+from sklearn.ensemble import RandomForestClassifier
+
+# Random Forest model
+rf_clf = RandomForestClassifier(n_estimators=300, max_depth=None, min_samples_split=2,
+      min_samples_leaf=1, class_weight='balanced_subsample', n_jobs=-1, random_state=42)
+
+# Model training with training dataset (X_train,y_train)
+rf_clf.fit(X_train, y_train)
+
+# Validation report over validation dataset (X_val,y_val)
+y_val_pred_rf = rf_clf.predict(X_val)
+print("Validation report (Random Forest on W2V embeddings):")
+print(classification_report(y_val, y_val_pred_rf))
+print("Validation Accuracy:", accuracy_score(y_val, y_val_pred_rf))
+
+# Final evaluation over test dataset (X_test,y_test)
+y_test_pred_rf = rf_clf.predict(X_test)
+print("Test report (Random Forest on W2V embeddings):")
+print(classification_report(y_test, y_test_pred_rf))
+print("Test Accuracy:", accuracy_score(y_test, y_test_pred_rf))
+print("Test Roc Auc Score: ", roc_auc_score(y_test, clf.predict_proba(X_test), multi_class='ovr'))
+print("Test F1 Score: ", f1_score(y_test, y_test_pred_rf, average='micro'))
 
 ##########################################
 # -----   PyTorch NN CLASSIFIER    ----- #
 ##########################################
 
-from sklearn.preprocessing import StandardScaler, LabelEncoder
 import torch
 from torch import nn
 from torch.optim import Adam
 from torch.utils.data import TensorDataset, DataLoader
 from W2V_torch_clf import *
-
-# Label Encoding ('True' -> 0, 'False' -> 1, 'Unverified' -> 2)
-label_encoder = LabelEncoder()
-Y_encoded = label_encoder.fit_transform(Y)
 
 # 60% -> Training dataset, 40% -> Test/validation datasets
 X_train_nn,X_temp_nn,y_train_nn,y_temp_nn = train_test_split(X_w2v,Y_encoded,test_size=0.4,random_state=42,stratify=Y_encoded) #type: ignore
